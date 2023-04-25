32daf0a817829c4d092991f18b67158517649b19
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-04-25 09:55:06 -0600

    Update roadtrip facade and test

--- spec/facades/roadtrip_facade_spec.rb
+++ spec/facades/roadtrip_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RoadtripFacade do
   it "can have a object" do
-    roadtrip = RoadtripFacade.new.find_travel_time("Denver,CO", "Boulder,CO")
+    roadtrip = RoadtripFacade.new.find_roadtrip_info("Denver,CO", "Boulder,CO")
 
     expect(roadtrip).to be_a(Roadtrip)
   end
