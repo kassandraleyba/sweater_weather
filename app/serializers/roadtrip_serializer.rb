777ccb20599d9eaa737eca4fcbb4ca88bdb11507
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-04-25 15:02:22 -0600

    Update serializer

--- app/serializers/roadtrip_serializer.rb
+++ app/serializers/roadtrip_serializer.rb
@@ -4,7 +4,4 @@ class RoadtripSerializer
   set_type :roadtrip #data, id, type portion
 
   attributes :start_city, :end_city, :travel_time, :weather_at_eta
-
-  # belongs_to :user
-  # set_id :id
 end
