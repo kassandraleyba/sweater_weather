e83a7d80e416eb2f5eff1694ece98c1b8c93c601
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-04-25 18:07:50 -0600

    Fix roadtrip facade method

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -13,33 +13,24 @@ class RoadtripFacade
     formatted_travel_time = travel_data[:route][:formattedTime]
 
     arrival_time = Time.now + travel_time #find weather at this time
-    # binding.pry
+ 
     # iterate all the days, make sure dates match
     # iterate through all the hours, and find the hour that matches the arrival time
     # create a hash with the data from that hour
-
-    forecast_days = forecast_data[:forecast][:forecastday].each do |day|
+    x = forecast_data[:forecast][:forecastday].each do |day|
       next unless arrival_time.to_s.include?(day[:date])
     
       day[:hour].each do |hour|
         hour_time = Time.parse(hour[:time])
         if arrival_time.between?(hour_time, hour_time + 1.hour)
           weather_at_eta = {
-            id: 'null',
-            type: 'roadtrip',
-            start_city: origin,
-            end_city: destination,
-            travel_time: formatted_travel_time,
-            weather_at_eta: {
-              datetime: hour[:time],
-              temperature: hour[:temp_f],
-              condition: hour[:condition][:text]
-            }
+            datetime: hour[:time],
+            temperature: hour[:temp_f],
+            condition: hour[:condition][:text]
           }
+          return Roadtrip.new(start_city, end_city, formatted_travel_time, weather_at_eta)
         end
-        Roadtrip.new(start_city, end_city, formatted_travel_time, weather_at_eta)
       end
-      binding.pry
     end
   end
 end
